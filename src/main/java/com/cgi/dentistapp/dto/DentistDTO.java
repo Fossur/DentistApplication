2ee3ef3b4ac3507fb5daa2d35a86111134de6ca3
Karl <karlhe@ttu.ee>
2021-04-26 15:14:05 +0300

    added dto objects

---
+++ src/main/java/com/cgi/dentistapp/dto/DentistDTO.java
@@ -0,0 +1,18 @@
+package com.cgi.dentistapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DentistDTO {
+
+    Long id;
+
+    String name;
+
+}
--- src/main/java/com/cgi/dentistapp/dto/DentistVisitDTO.java
+++ src/main/java/com/cgi/dentistapp/dto/DentistVisitDTO.java
@@ -1,41 +1,28 @@
 package com.cgi.dentistapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DentistVisitDTO {
+
+    Long id;
 
     @Size(min = 1, max = 50)
     String dentistName;
 
-    @NotNull
-    @DateTimeFormat(pattern = "dd.MM.yyyy")
-    Date visitTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    Date visitDate;
 
-    public DentistVisitDTO() {
-    }
+    String visitTime;
 
-    public DentistVisitDTO(String dentistName, Date visitTime) {
-        this.dentistName = dentistName;
-        this.visitTime = visitTime;
-    }
-
-    public String getDentistName() {
-        return dentistName;
-    }
-
-    public void setDentistName(String dentistName) {
-        this.dentistName = dentistName;
-    }
-
-    public Date getVisitTime() {
-        return visitTime;
-    }
-
-    public void setVisitTime(Date visitTime) {
-        this.visitTime = visitTime;
-    }
 }
